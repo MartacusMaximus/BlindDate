fb3a9facdbade40a7e8117a91e9c595e4878516c
DESKTOP-GDCD3KN\vande <vanderhoekmart@gmail.com>
2018-11-06 15:25:34 +0100

    PlayerPickup
    Pickup of object

---
+++ BlindDate/Assets/Scripts/PickupSoundplayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickup : MonoBehaviour
+{
+    private GameObject pickedupObject;
+    private Rigidbody pickedupRigidbody;
+    public AudioClip collectSound;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (pickedupObject == null)
+            {
+                int layerMask = 1 << 10;
+                RaycastHit hitInfo;
+                if (Physics.Raycast(transform.position + transform.forward, transform.forward, out hitInfo, 3.0f, layerMask))
+                {
+                    pickedupObject = hitInfo.collider.gameObject;
+
+                    pickedupObject.transform.parent = transform;
+                    AudioSource.PlayClipAtPoint(collectSound, transform.position);
+                    pickedupRigidbody = (Rigidbody)pickedupObject.GetComponent(typeof(Rigidbody));
+                    pickedupRigidbody.isKinematic = true;
+
+                }
+            }
+            else
+            {
+                pickedupRigidbody.isKinematic = false;
+                pickedupObject.transform.parent = null;
+                pickedupObject = null;
+
+            }
+        }
+    }
+}
