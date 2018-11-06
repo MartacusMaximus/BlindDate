9e01daa1515f829fc4aaafd18c83bc7194886ca5
Unknown <jancarlpeters@gmail.com>
2018-11-06 16:33:05 +0100

    kut

--- BlindDate/Assets/Scripts/Paint.cs
+++ BlindDate/Assets/Scripts/Paint.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Paint : MonoBehaviour {
 
     public GameObject stroke;
     public int refreshRate;
-
     private int frames = 0;
 
     // Use this for initialization
@@ -18,16 +18,17 @@ public class Paint : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (frames == refreshRate)
+		if (Input.GetAxis("LTrigger")>0)
         {
-            if (Input.GetAxis("10") == 1.0)                 //right controller trigger held
+			if (frames == refreshRate)               
             {
                 frames = 0;
                 Vector3 pos = this.transform.position;
                 Quaternion rot = this.transform.rotation;
                 Instantiate(stroke, pos, rot);
+				
             }
+			frames++;
         }
-        frames++;
 	}
 }
