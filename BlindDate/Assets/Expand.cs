9ad847fcc51d7c0ec4c12699d4a27dc0d9934822
DESKTOP-GDCD3KN\vande <vanderhoekmart@gmail.com>
2018-11-07 16:52:04 +0100

    Ballon Script
    Script voor ballonnen

---
+++ BlindDate/Assets/Expand.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Expand : MonoBehaviour {
+
+    bool Expanding = false;
+    float timer = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            if (timer > 0)
+            {
+                transform.localScale += new Vector3(0.05f, 0.05f, 0.05f);
+                timer = timer - Time.deltaTime;
+            }
+        }
+        else
+        {
+            Expanding = false;
+            timer = 0.2f;
+        }
+		
+	}
+}
---
+++ BlindDate/Assets/Scripts/Balloon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    GameObject ballon;
+    public GameObject ballonPrefab;
+    public Vector3 location;
+    bool Create = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && Create == false)
+        {
+            if (ballon != null)
+            {
+                ballon.GetComponent<Expand>().enabled = false;
+            }
+            ballon = Instantiate(ballonPrefab, location, Quaternion.identity);
+            Create = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Create = false;
+        }
+    }
+}
