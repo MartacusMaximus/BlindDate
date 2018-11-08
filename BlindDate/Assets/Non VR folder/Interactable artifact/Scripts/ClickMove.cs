b211f2f22ee5b81108f5a264754245b48e12b443
Unknown <Sorasak.Absolute@gmail.com>
2018-11-08 15:41:35 +0100

    Test controllers

--- BlindDate/Assets/Non VR folder/Interactable artifact/Scripts/ClickMove.cs
+++ BlindDate/Assets/Non VR folder/Interactable artifact/Scripts/ClickMove.cs
@@ -5,37 +5,38 @@ using UnityEngine;
 public class ClickMove : MonoBehaviour {
 
 	float distance = 1.25f;
-	public AudioClip TestSound;
-	//bool isPlaying = false;
+
+	public AudioClip Artifact;
+	public float Volume;
+	AudioSource audioSource;
+	public bool SoundPlayed = false;
 
 	void OnMouseDrag () {
 		Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
 		Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
 		transform.position = objPosition;
 
-			//if (isPlaying != true) {
-			//	AudioSource.PlayClipAtPoint (TestSound, transform.position);
-			//	isPlaying = true;
-		//	} 
-		//	else
-		//	{
-		//		isPlaying = false;
-		//	}
 
 	}
-	public class OnMouseOverExample : MonoBehaviour
+
+	void Start(){
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	void OnMouseDown ()
 	{
-		void OnMouseOver()
-		{
-			//If your mouse hovers over the GameObject with the script attached, output this message
-			Debug.Log("Mouse is over GameObject.");
-		}
-
-		void OnMouseExit()
-		{
-			//The mouse is no longer hovering over the GameObject so output this message each frame
-			Debug.Log("Mouse is no longer on GameObject.");
-
+		if (!SoundPlayed) {
+			audioSource.PlayOneShot (Artifact, Volume);
+			SoundPlayed = true;
 		}
 	}
+
+	void OnMouseUp ()
+	{
+		if (SoundPlayed == true) {
+			audioSource.Stop();
+			SoundPlayed = false;
+		}
+	}
+
 }
---
+++ BlindDate/Assets/Non VR folder/Interactable artifact/Scripts/ClickMoveVR.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+	public class ClickMoveVR : MonoBehaviour {
+
+		float distance = 1.25f;
+
+		public AudioClip Artifact;
+		public float Volume;
+		AudioSource audioSource;
+		public bool SoundPlayed = false;
+
+		public SteamVR_Action_Boolean PickupAction;
+
+		void Start(){
+			audioSource = GetComponent<AudioSource> ();
+		}
+
+		void Update()
+		{
+			if (PickupAction.GetState(SteamVR_Input_Sources.Any))
+			{
+				Vector3 Pos = this.transform.position;
+				Quaternion rot = this.transform.rotation;
+			}
+		}
+
+
+		void OnMouseDrag () {
+			Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+			Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+			transform.position = objPosition;
+
+
+		}
+
+		void OnMouseDown ()
+		{
+			if (!SoundPlayed) {
+				audioSource.PlayOneShot (Artifact, Volume);
+				SoundPlayed = true;
+			}
+		}
+
+		void OnMouseUp ()
+		{
+			if (SoundPlayed == true) {
+				audioSource.Stop();
+				SoundPlayed = false;
+			}
+		}
+
+	}
+}
