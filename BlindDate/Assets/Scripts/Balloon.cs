ecc9dbcc9550cb94b9ad5423e69966f800747306
Unknown <jancarlpeters@gmail.com>
2018-11-08 13:29:00 +0100

    Balloons!!!

--- BlindDate/Assets/Scripts/Balloon.cs
+++ BlindDate/Assets/Scripts/Balloon.cs
@@ -1,36 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
-public class Balloon : MonoBehaviour
+namespace Valve.VR.InteractionSystem.Sample
 {
-    GameObject ballon;
-    public GameObject ballonPrefab;
-    public Vector3 location;
-    bool Create = false;
-    private float time;
-    // Use this for initialization
-    void Start()
-    {
+	public class Balloon : MonoBehaviour
+	{
+	    GameObject ballon;
+	    public GameObject ballonPrefab;
+		public SteamVR_Action_Boolean balloonAction;
+	//    public Vector3 location;
+	    bool Create = false;
+	    // Use this for initialization
+	    void Start()
+	    {
 
-    }
+	    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButton(0) && Create == false)
-        {
-            if (ballon != null)
-            {
-                ballon.GetComponent<Expand>().enabled = false;
-            }
-            ballon = Instantiate(ballonPrefab, location, Quaternion.identity);
-            Create = true;
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            time++;
-            Create = false;
-        }
-    }
+	    // Update is called once per frame
+	    void Update()
+	    {
+			if (balloonAction.GetState(SteamVR_Input_Sources.Any) && Create == false)
+	        {
+	            if (ballon != null)
+	            {
+	                ballon.GetComponent<Expand>().enabled = false;
+	            }
+				Vector3 location = this.transform.position;
+	            ballon = Instantiate(ballonPrefab, location, Quaternion.identity);
+	            Create = true;
+	        }
+			if (balloonAction.GetState(SteamVR_Input_Sources.Any)==false)
+	        {
+	            Create = false;
+	        }
+	    }
+	}
 }
